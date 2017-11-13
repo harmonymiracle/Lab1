26c181c98172fe92118eb1cbe646b6e9a33d7395
harmonymiracle <1459078670@qq.com>
2017-11-13 14:01:14 +0800

    black box test

--- lab_one/src/lab_one/MyGraphTest.java
+++ lab_one/src/lab_one/MyGraphTest.java
@@ -5,7 +5,48 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MyGraphTest {
+	//black box test
+	private static MyGraph graph = new MyGraph();
+	@Before
+	public void setUp() throws Exception {
+		graph.LoadTestText();
+	}
 
+	@Test
+	public void testQueryBridgeWords() {
+		graph.queryBridgeWords("my", "target");
+		assertEquals("main", graph.Get_Bridge());
+		
+		graph.queryBridgeWords("me", "empty");
+		assertEquals("feel", graph.Get_Bridge());
+		
+		graph.queryBridgeWords("have", "nothing");
+		assertEquals("done", graph.Get_Bridge());
+		
+		graph.queryBridgeWords("then", "make");
+		assertEquals("", graph.Get_Bridge());
+		
+		graph.queryBridgeWords("I", "a");
+		assertEquals("set", graph.Get_Bridge());
+
+		graph.queryBridgeWords("is", "for");
+		assertEquals("needed time", graph.Get_Bridge());
+		
+		graph.queryBridgeWords("needed", "me");
+		assertEquals("for", graph.Get_Bridge());
+		
+		graph.queryBridgeWords("all", "the");
+		assertEquals("", graph.Get_Bridge());
+		
+		graph.queryBridgeWords("the", "makes");
+		assertEquals("time", graph.Get_Bridge());
+		
+		graph.queryBridgeWords("make", "happy");
+		assertEquals("", graph.Get_Bridge());	
+		//fail("Not yet implemented");
+	}
+	
+	// white box test
     @Test
     public void testGenerateNewText() {
         MyGraph graph = new MyGraph ();
